045800254f527e5fbd3695073eeca24cb48b6040
ckrenslehner <ckrenslehner@gmail.com>
2022-10-23 11:27:39 +0200

    Add choco install script

---
+++ choco_install_tools.ps1
@@ -0,0 +1,5 @@
+choco install cmake --version 3.24.2 -y
+choco install ninja --version 1.11.1 -y
+choco install mingw --version 12.2.0 -y
+choco install openocd --version 0.11.0.20211118 -y
+choco install gcc-arm-embedded --version 10.2.1 -y
